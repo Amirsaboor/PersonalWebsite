a947681410310fb636460d92244b3bdbc8ad61ef
Amirsaboor <amir.saboor777@gmail.com>
2025-08-15 01:45:33 +0330

    add fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,12 @@
 import { Inter } from 'next/font/google'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
-
 import '@/styles/tailwind.css'
 import 'focus-visible'
+import './global.css'
 
-const inter = Inter({ subsets: ['latin'] })
+
+
 
 export const metadata = {
   title: 'Name Family - Leader, artist, and frontend enthusiast',
@@ -15,17 +16,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full">
-      <body className={`${inter.className} h-full bg-white  dark:bg-black dark:ring-zinc-300/20`}>
-        <div className="fixed inset-0 flex justify-center sm:px-8 ">
-          <div className="flex w-full max-w-7xl lg:px-8 ">
+      <body className={`h-full bg-white dark:bg-black dark:ring-zinc-300/20 `} dir='rtl'>
+        <div className="fixed inset-0 flex justify-center sm:px-8">
+          <div className="flex w-full max-w-7xl lg:px-8">
             <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
           </div>
         </div>
         <div className="relative">
           <Header />
-          <main>
-            {children}
-          </main>
+          <main>{children}</main>
           <Footer />
         </div>
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -14,14 +14,14 @@ export default function Home() {
     <>
       <Container className="mt-9">
         <div className="max-w-2xl">
-          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Leader, artist, and frontend enthusiast in Austin, TX.
+          <h1 className="text-2xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-4xl">
+            برنامه‌نویس FrontEnd و طراح سایت
           </h1>
-          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+          <p className=" mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I&apos;m Victoria, a computer science student and the Founder and
             President of Girls Who Code at Texas State University. I am
             passionate about frontend engineering, empowering women in tech, and
-            creating inclusive spaces for beginners in computer science.
+            creating inclusive spaces for beginners in computer science
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
@@ -39,7 +39,10 @@ export default function Home() {
       </Container>
       <Photos />
       <Container className="mt-24 md:mt-28">
-        <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
+        <div className="mx-auto grid max-w-xl grid-cols-2 gap-y-20 lg:max-w-none lg:grid-cols-2">
+          <div className="space-y-10 lg:pl-16 xl:pl-24">
+            <Resume />
+          </div>
           <div className="space-y-10 lg:pl-16 xl:pl-24">
             <Resume />
           </div>
